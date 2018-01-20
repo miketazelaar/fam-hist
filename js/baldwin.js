cef319a20493eec60ed6e5efde98ad1aa2979571
mtazelaar <34174512+mtazelaar@users.noreply.github.com>
2018-01-20 14:14:44 -0700

    family history files
    commit to master initial files

---
+++ js/baldwin.js
@@ -0,0 +1,56 @@
+var baldwin = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.704405,
+          53.763225
+        ]
+      },
+      "properties": {
+        "title": "John Baldwin Sr.",
+        "marker-color": "#ff0000",
+        "marker-size": "small",
+        "description": "John Baldwin was born in 1630"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.829674,
+          36.089987
+        ]
+      },
+      "properties": {
+        "title": "John Baldwin",
+        "marker-color": "#ff0000",
+        "marker-size": "x-small",
+        "description": "John Baldwin was born in 1670"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "stroke": "#ff0000"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -2.704405,
+            53.763225
+          ],
+          [
+            -79.829674,
+            36.089987
+          ]
+        ]
+      }
+    }
+  ]
+};
