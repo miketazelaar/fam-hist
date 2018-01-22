7af69119e876ad93bf21ac38d80efa0aa43fc542
miketazelaar <35588823+miketazelaar@users.noreply.github.com>
2018-01-21 19:02:03 -0700

    Add files via upload
    add folders and files for map program

---
+++ docs/js/baldwin.js
@@ -0,0 +1,94 @@
+var baldwin = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.704405,
+          53.763225
+        ]
+      },
+      "properties": {
+        "title": "John Baldwin Sr.",
+        "marker-color": "#ff0000",
+        "marker-size": "small",
+        "description": "John Baldwin was born in 1630"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.829674,
+          36.089987
+        ]
+      },
+      "properties": {
+        "title": "John Baldwin",
+        "marker-color": "#ff0000",
+        "marker-size": "x-small",
+        "description": "John Baldwin was born in 1670"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.192355, 41.203330
+        ]
+      },
+      "properties": {
+        "title": "William Baldwin",
+        "marker-color": "#ff0000",
+        "marker-size": "x-small",
+        "description": "William Baldwin was born in 1720 in Pennsylvania, USA"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -78.260102, 39.209644//Virginia
+        ]
+      },
+      "properties": {
+        "title": "John Baldwin",
+        "marker-color": "#ff0000",
+        "marker-size": "x-small",
+        "description": "John Baldwin was born in 1746 in Hopeville,Frederick,Virginia, USA"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "stroke": "#ff0000"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -2.704405,
+            53.763225
+          ],
+          [
+            -79.829674,
+            36.089987
+          ],
+          [
+            -77.192355,
+             41.203330
+          ],
+          [
+            -78.260102,
+             39.209644
+          ]
+        ]
+      }
+    }
+  ]
+};
